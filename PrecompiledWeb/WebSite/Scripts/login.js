46101f4fb80effcdbf79321e36b3bba788c8d314
lythen <lyrenlian@163.com>
2015-07-09 16:28:22 +0800

    删除发布的网站

--- PrecompiledWeb/WebSite/Scripts/login.js
+++
@@ -1,8 +0,0 @@
-﻿function checkLogin(data) {
-    if (data == "nologin") {
-        alert("没有登陆或登陆超时，请重新登陆。");
-        top.location.href = "login.html";
-        return false;
-    }
-    return true;
-}
